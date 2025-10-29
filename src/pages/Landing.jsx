baa4bbdff993700f69cc1e632d2445a7c9ca4f89
hadeersalah <hadeersalah127@gmail.com>
2025-10-29 22:21:04 +0300

    adding main routes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
@@ -13,7 +14,7 @@ const Landing = () => {
                     <div className="info">
                         <h1>job <span>tracking</span> app</h1>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam placeat accusamus atque animi! Corrupti excepturi iste nihil adipisci debitis voluptatibus enim, culpa repellendus minima. Debitis quos eveniet animi obcaecati necessitatibus.</p>
-                        <button className="btn btn-hero">login/register</button>
+                        <Link to="/register" className="btn btn-hero">login/register</Link>
                     </div>
                     <img src={main} alt="main hero" />
                 </div>
