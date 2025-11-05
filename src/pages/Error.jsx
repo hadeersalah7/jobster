137a5db6fc15f0b1681f7f2a1e36d56db17e3e6d
Hadeer Salah <hadeersalah127@gmail.com>
2025-11-05 22:01:28 +0200

    adding error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import ErrorPageWrapper from '../assets/wrappers/ErrorPage'
+import img from '../assets/images/not-found.svg'
+import { Link } from 'react-router-dom'
 const Error = () => {
   return (
-    <div>Error</div>
+    <ErrorPageWrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Ohh! page not found</h3>
+        <p>We can't find the page that you're looking for!</p>
+        <Link to={"/"}>go home</Link>
+      </div>
+
+    </ErrorPageWrapper>
   )
 }
 
