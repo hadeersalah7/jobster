ce0d59103468342495b7ed25dffa2bb51fdb8cd6
Hadeer Salah <hadeersalah127@gmail.com>
2025-11-10 20:55:24 +0200

    saving and getting user from localStorage and adding login thunk

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,50 +1,83 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import fetchClient from "../../utils/axios";
 import { toast } from "react-toastify";
+import {
+    getUserFromLocalStorage,
+    saveUserToLocalStorage,
+} from "../../utils/localStorage";
 
 const initialState = {
     isLoading: false,
-    user: null
-}
+    user: getUserFromLocalStorage(),
+};
 
-// login API Thunk: 
+// login API Thunk:
 export const loginUser = createAsyncThunk(
-    '/user/loginUser', async (user, thunkAPI) => {
-        console.log(`Login User:  ${user}`)
-    })
-
-// Register User Thunk: 
-export const registerUser = createAsyncThunk(
-    "user/registerUser", async (user, thunkAPI) => {
+    "/user/loginUser",
+    async (user, thunkAPI) => {
         try {
-            const response = await fetchClient.post("auth/register", user)
-            return response?.data
+            const response = await fetchClient.post("auth/login    ", user);
+            return response?.data;
         } catch (error) {
-            console.error(error)
-            thunkAPI.rejectWithValue(error.message)
+            console.error(error);
+            return thunkAPI.rejectWithValue(error.response?.data?.msg);
         }
     }
-)
+);
 
+// Register User Thunk:
+export const registerUser = createAsyncThunk(
+    "user/registerUser",
+    async (user, thunkAPI) => {
+        try {
+            const response = await fetchClient.post("auth/register", user);
+            return response?.data;
+        } catch (error) {
+            console.error(error);
+            return thunkAPI.rejectWithValue(error.response?.data?.msg);
+        }
+    }
+);
 
 const userSlice = createSlice({
     name: "user",
     initialState,
     extraReducers: (builder) => {
-        builder.addCase(registerUser.pending, (state) => {
-            state.isLoading = true
-        }).addCase(registerUser.fulfilled, (state, { payload }) => {
-            state.isLoading = false
-            // const { user } = payload
-            if (payload) {
-                state.user = payload
-                toast.success(`Welcome ${payload.user.name}! ^_^`)
-            }
-
-        }).addCase(registerUser.rejected, (state, { payload }) => {
-            state.isLoading = false;
-            if (payload) toast.error("error", payload)
-        })
-    }
-})
-export default userSlice.reducer
+        // register cases:
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, { payload }) => {
+                state.isLoading = false;
+                // const { user } = payload
+                if (payload) {
+                    state.user = payload;
+                    saveUserToLocalStorage(payload);
+                    toast.success(`Welcome ${payload.user.name}! ^_^`);
+                }
+            })
+            .addCase(registerUser.rejected, (state, { payload }) => {
+                state.isLoading = false;
+                if (payload) toast.error(payload);
+            });
+        // Login cases:
+        builder
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(loginUser.fulfilled, (state, { payload }) => {
+                state.isLoading = false;
+                if (payload) {
+                    state.user = payload;
+                    saveUserToLocalStorage(payload);
+                    toast.success(`Welcome ${payload.user.name}! ^_^`);
+                }
+            })
+            .addCase(loginUser.rejected, (state, { payload }) => {
+                state.isLoading = false;
+                if (payload) toast.error(payload);
+            });
+    },
+});
+export default userSlice.reducer;
