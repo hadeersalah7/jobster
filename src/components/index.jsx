42a02724c688e2d1bccf720e073615fb305b4f37
Hadeer Salah <hadeersalah127@gmail.com>
2025-12-28 11:56:55 +0200

    making the navlinks a seperate component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,7 @@ import BigSidbar from "./BigSidbar";
 import FormRow from "./FormRow";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
+import NavLinks from "./NavLinks";
 import SmallSidebar from "./SmallSidebar";
 
-export { Logo, FormRow, BigSidbar, SmallSidebar, Navbar };
+export { Logo, FormRow, BigSidbar, SmallSidebar, Navbar, NavLinks };
